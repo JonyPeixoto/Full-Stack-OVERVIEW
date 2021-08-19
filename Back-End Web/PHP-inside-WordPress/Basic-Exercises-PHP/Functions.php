868acd9bb6233ed6ba1a6b19fdb9f8f6856d76d8
JonyPeixoto <jonathanppeixoto@gmail.com>
2021-08-19 10:59:55 -0300

    update-back-end-projects

---
+++ Back-End Web/PHP-inside-WordPress/Basic-Exercises-PHP/Functions.php
@@ -0,0 +1,25 @@
+<?php
+
+	/* EXAMPLE 01 - FUNCTIONS */
+
+	/*
+	
+	function tellCybertimeUP(){
+		return 'Hello world of the CybertimeUP';
+	}
+
+	echo tellCybertimeUP();
+
+	*/
+
+
+	/* EXAMPLE 02 - FUNCTIONS */
+
+
+	function tellCybertimeUP(){
+		echo 'tellCybertimeUP';
+	}
+
+	tellCybertimeUP();
+
+?>
